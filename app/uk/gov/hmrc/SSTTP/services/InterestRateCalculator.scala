1768118dc84062414a7f616d2c56cfef02566d1e
ZDevelop94 <zat3000@gmail.com>
2016-08-25 09:20:53 +0100

    changes

--- app/uk/gov/hmrc/SSTTP/services/InterestRateCalculator.scala
+++ app/uk/gov/hmrc/SSTTP/services/InterestRateCalculator.scala
@@ -8,12 +8,14 @@ import uk.gov.hmrc.play.microservice.controller.BaseController
 trait InterestRateCalculator extends BaseController{
 
   val name: String = "Bob"
+  val amount : Double = 1233.00
   val interestRate: Double = 0.015
   val Days:Int = 23
   val result: Long
-  val rounded: Double
-  def CalculateInterest(amount: Double, rate: Double, numberDays: Int) =  {
-    val calculator = (amount * rate * numberDays) / 36600
+  val roundedCalculation = BigDecimal(CalculateInterest(amount, interestRate, Days)).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+  def CalculateInterest(amount: Double, rate: Double, numberDays: Int): Double = {
+    val result: Double = (amount * rate * numberDays) / 36600
+    result
   }//number 36600 used for calculation
 
 }
